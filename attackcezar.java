6b3235995cbc2184b7605fffdc70b15e393ca8d5
EldaDrenica <123164394+EldaDrenica@users.noreply.github.com>
2023-04-16 16:32:40 +0200

    Update attackcezar.java

--- attackcezar.java
+++ attackcezar.java
@@ -18,3 +18,15 @@ public class attackcezar {
         String plaintext = Dekriptimi(ciphertext);
         System.out.println("Teksti i dekriptuar: " + plaintext);
     }
+public static String sms = "";
+
+public static String Dekriptimi(String ciphertext) {
+    ciphertext = ciphertext.toUpperCase();
+    char[] alfabeti = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    char[] mesazhiEch = ciphertext.toCharArray();
+    String[] tekstiKontrollues = {"AND", "IS"};
+
+    String[] Split_Mesazhi = ciphertext.split(" ");
+
+    List<String> list = new ArrayList<String>();
